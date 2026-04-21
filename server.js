ff6578c4d87ff0d907b0f6928db3680826b0f655
Nargiza <alijonovanargiza38@gmail.com>
2026-04-21 08:36:47 +0500

    BRR:building express web freamwork

---
+++ server.js
@@ -0,0 +1,29 @@
+console.log("Web serverni boshlash");
+const express = require('express');
+const app = express();
+const http = require('http')
+
+//1 Kirish kodlari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2 session code
+
+//3 Views code
+app.set("views","views");
+app.set("view enginee","ejs");
+
+//4 Routing code
+app.get("/",function(req,res){
+res.end(`<h1 style="background:red">Hello world by Diana</h1>`);
+});
+app.get("/gift",function(req,res){
+res.end(`<h1 style="background:pink">siz sovg'alar bo'limidasiz</h1>`);
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server running successfully on port: ${PORT}`);
+});
